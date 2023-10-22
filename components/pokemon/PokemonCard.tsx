b10aba2999cfc2b9f12f92faa55c9fa6abdde186
alejandro221996 <alejandro1996221@gmail.com>
2023-10-21 23:45:43 -0600

    Add new feature badge for types

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,24 +1,40 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import { Grid, Card, Row, Text } from "@nextui-org/react";
 
-import { SmallPokemon } from "../../interfaces";
-import { getTextGradient } from "../../utils/getTypeColor";
+import { PropsPokemon, SmallPokemon } from "../../interfaces";
+import { getStylesByType } from "../../utils/getTypeColor";
+import { getPokemonInfo } from "../../utils";
 interface Props {
   pokemon: SmallPokemon;
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
-  console.log(pokemon);
+  const { id, img, name } = pokemon;
+  const [pokemonInfo, setPokemonInfo] = useState<PropsPokemon | null>(null);
   const router = useRouter();
   const onClick = () => {
-    router.push(`/name/${pokemon.name}`);
+    router.push(`/name/${name}`);
   };
   /* const textGradient = getTextGradient(pokemon.types); */
-
+  useEffect(() => {
+    getPokemonInfo(id.toString()).then((data) => {
+      if (data) {
+        setPokemonInfo({
+          id: data.id,
+          name: data.name,
+          sprites: [data.sprites],
+          types: data.types,
+          stats: data.stats,
+          weight: data.weight,
+          height: data.height,
+        });
+      }
+    });
+  }, [id]);
   return (
-    <Grid xs={6} sm={3} md={2} xl={3} key={pokemon.id}>
+    <Grid xs={6} sm={3} md={2} xl={3} key={id}>
       <Card
         hoverable
         clickable
@@ -26,14 +42,30 @@ export const PokemonCard: FC<Props> = ({ pokemon }) => {
         css={{ border: "2px white solid" }}
       >
         <Card.Body css={{ p: 1 }}>
-          <Card.Image src={pokemon.img} width="100%" height={140} />
+          <Card.Image src={img} width="100%" height={140} />
         </Card.Body>
         <Card.Footer>
-          <Row justify="center">
-            <Text weight="bold" transform="capitalize">
-              {pokemon.name}
-            </Text>
-          </Row>
+          <Grid.Container gap={2} justify="center" direction="column">
+            <Grid xs={12} justify="center">
+              <Text weight="bold" transform="capitalize">
+                {name}
+              </Text>
+            </Grid>
+            <Grid xs={12}>
+              <Grid.Container gap={1} justify="center">
+                {pokemonInfo?.types.map((type) => (
+                  <Grid xs={12} sm={6} key={type.type.name} justify="center">
+                    <Text
+                      style={getStylesByType(type.type.name)}
+                      transform="capitalize"
+                    >
+                      {type.type.name}
+                    </Text>
+                  </Grid>
+                ))}
+              </Grid.Container>
+            </Grid>
+          </Grid.Container>
         </Card.Footer>
       </Card>
     </Grid>
--- utils/getTypeColor.ts
+++ utils/getTypeColor.ts
@@ -18,3 +18,77 @@ export const getTextGradient = (type: string) => {
       return "45deg, $white -20%, $green600 100%"; // Add a default style here
   }
 };
+
+export const getStylesByType = (type: string) => {
+  const typeStyles = {
+    fire: {
+      backgroundColor: "red",
+      color: "white",
+    },
+    grass: {
+      backgroundColor: "green",
+      color: "white",
+    },
+    water: {
+      backgroundColor: "#4592c4",
+      color: "white",
+    },
+    electric: {
+      backgroundColor: "yellow",
+      color: "black",
+    },
+    ground: {
+      backgroundColor: "#f7de3f",
+      color: "black",
+    },
+    rock: {
+      backgroundColor: "#a38c21",
+      color: "white",
+    },
+    ice: {
+      backgroundColor: "blue",
+      color: "white",
+    },
+    fairy: {
+      backgroundColor: "pink",
+      color: "white",
+    },
+    poison: {
+      backgroundColor: "purple",
+      color: "white",
+    },
+    bug: {
+      backgroundColor: "#9bcc50",
+      color: "white",
+    },
+    fighting: {
+      backgroundColor: "#d56723",
+      color: "white",
+    },
+    psychic: {
+      backgroundColor: "#f366b9",
+      color: "white",
+    },
+    steel: {
+      backgroundColor: "#9eb7b8",
+      color: "white",
+    },
+    default: {
+      backgroundColor: "white",
+      color: "black",
+    },
+  };
+
+  const selectedStyle =
+    typeStyles[type as keyof typeof typeStyles] || typeStyles.default;
+
+  // Merge the selected style with defaultStyle
+  return {
+    borderRadius: 5,
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingTop: 4,
+    paddingBottom: 4,
+    ...selectedStyle,
+  };
+};
